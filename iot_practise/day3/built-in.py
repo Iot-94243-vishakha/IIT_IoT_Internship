5199c1ced8ace55471ced1118070dba78078e132
vishakha607 <23uet141@dkte.ac.in>
2025-12-15 11:10:48 +0530

    first

---
+++ iot_practise/day3/built-in.py
@@ -0,0 +1,11 @@
+word1 = ("Vishakha Sudhir Sapkal")
+word2 = ("Pranali Pravin Bhosale")
+print(word1.lower())#converts into lower case
+print(word2.upper())#converts int upper case
+print(word1.replace("Vishakha","gopi"))#replacing vichakha by gopi
+print("".join("Sunbeam"))#add extra sunbeam
+print("word2:{} \nword1:{}".format(word2,word1))#exchanging names
+print(word2.isalpha())#if all letters are arranged alphabetically
+print(word2.split())#split each word with specfic space
+print(word1.isascii())#if ascii value then true 
+print(word2.find("B"))#find number of given letter 
---
+++ iot_practise/day3/stringslicing.py
@@ -0,0 +1,8 @@
+s = input("Enter a string: ")
+print("Original String:",s)
+print("First 5 characters:",s[0:5])#first 5 letters
+print("Character from index 2 to 7:",s[2:8])#letters from 2nd letter to 7th letter
+print("Last 5 characters:",s[-5:])
+print("String without first & last character:",s[1:-1])
+print("Every second character:",s[::2])
+print("Reverse string:",s[::-1])
